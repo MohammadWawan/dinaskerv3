403667d9f0f3374af8aa32239faa180c8735ed1c
Mohammad Wawan <40724858+sompretofimalahua@users.noreply.github.com>
2021-03-07 05:50:56 +0700

    third commit

---
+++ siapelfix/app/src/main/java/com/example/siapel/homepage.java
@@ -0,0 +1,41 @@
+package com.example.siapel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage_activity);
+
+        ImageSlider imageSlider= findViewById(R.id.slider);
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel("https://1.bp.blogspot.com/-Njv0pYX0xeI/W2MhKOyffjI/AAAAAAAACj4/MQLLzqm30AwbfMlByKyck9psB6vn_QIYwCLcBGAs/s640/IMG-20180802-WA0445.jpg","Dispenduk Kota Malang"));
+        slideModels.add(new SlideModel("https://cdns.klimg.com/newshub.id/news/2017/01/05/113557/750x500-tenaga-outsourcing-ditarik-layanan-kependudukan-di-kota-malang-kacau-170105x.jpg","Dispenduk Kota Malang"));
+        slideModels.add(new SlideModel("https://bangsaonline.com/images/uploads/berita/700/5f40816a32ee830229e8fe348c3f0b54.jpg","Dispenduk Kota Malang"));
+        slideModels.add(new SlideModel("https://www.klikapa.com/images/2020-12-089-12-adm-dikcapil.jpg","Dispenduk Kota Malang"));
+        imageSlider.setImageList(slideModels,true);
+
+        ImageButton konsolidasi = (ImageButton) findViewById(R.id.bt8);
+        konsolidasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b_konsolidasi = new Intent(homepage.this,persyaratan.class);
+                startActivity(b_konsolidasi);
+            }
+        });
+    }
+}
